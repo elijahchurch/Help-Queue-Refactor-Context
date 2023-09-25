ea904d625d47974f9e9dac7aac3476f0d4d90338
Suzanne <suzanneschuber@gmail.com>
2023-09-25 11:20:36 -0700

    Add context to child components, practicing various different methods to do so co-authored by: Jason Church elijahchurch@gmail.com

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -2,17 +2,26 @@ import React from "react";
 import { PropTypes } from "prop-types";
 
 function ToggleTheme(props) {
-    const { toggleTheme} = props;
+    const { theme, toggleTheme} = props;
+
+    const styles = {
+        backgroundColor: theme.buttonBackground,
+        color: theme.textColor
+    }
+
     return (
         <React.Fragment>
-            <button onClick={toggleTheme}>Toggle Theme</button>
+            <button style={styles} onClick={toggleTheme}>
+                {theme.textColor === "AntiqueWhite" ? "toggle light theme" : "toggle dark theme"}
+            </button>
             <hr />
         </React.Fragment>
     );
 }
 
 ToggleTheme.propTypes = {
-    toggleTheme: PropTypes.func
+    toggleTheme: PropTypes.func,
+    theme: PropTypes.object
 }
 
 export default ToggleTheme;
